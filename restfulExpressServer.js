997bcb5d3e26b33e0000db8fe24758cd9d54b69c
AdamDanielJones <adamdjonesbusiness@gmail.com>
2022-10-05 18:01:51 -0400

    adds CRUD

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,11 +1,12 @@
 const PORT = 3000;
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const {Client} = require('pg');
-const connectionString = 'postgresql://postgres:docker@127.0.0.1:5432/pet_shop'
+const config = require('./node_modules/config.json')[process.env.NODE_ENV||"dev"]
 
 const client = new Client({
-    connectionString: connectionString,
+    connectionString: config.connectionString,
 })
 
 client.connect();
@@ -13,32 +14,37 @@ client.connect();
 app.use(express.json());
 
 app.get('/pets', (req, res)=> {
+
     client.query(`SELECT * FROM pets`)
     .then(result => {
+        res.status(200)
         res.send(result.rows);
     })
 })
 
 app.get('/pets/:id', (req, res)=> {
     var input = req.params.id
-    client.query(`SELECT * FROM pets WHERE pets_id=${input}`)
+    client.query(`SELECT * FROM pets WHERE pets_id=$1`, [input])
     .then(result => {
+        res.status(200)
         res.send(result.rows);
     })
 })
 
 app.get('/pets/name/:name', (req, res)=> {
     var input = req.params.name
-    client.query(`SELECT * FROM pets WHERE name='${input}'`)
+    client.query(`SELECT * FROM pets WHERE name=$1`, [`${input}`])
     .then(result => {
+        res.status(200)
         res.send(result.rows);
     })
 })
 
 app.get('/pets/age/:age', (req, res)=> {
     var input = req.params.age
-    client.query(`SELECT * FROM pets WHERE age=${input}`)
+    client.query(`SELECT * FROM pets WHERE age=$1`, [`${input}`])
     .then(result => {
+        res.status(200)
         res.send(result.rows);
     })
 })
@@ -48,41 +54,42 @@ app.post('/pets/create', (req, res) => {
     var nameInput = req.body.name;
     var ageInput = req.body.age;
     var kindInput = req.body.kind;
+    if (!nameInput || !ageInput || !kindInput){
+        res.status(400)
+        res.send('ERROR ~ Please provide all property inputs')
+    }
+    if (typeof ageInput !== 'number'){
+        res.status(400)
+        res.send('Age must be a number')
+    }
     client.query(`INSERT INTO pets (name, age, kind) VALUES ($1, $2, $3)`, [nameInput, ageInput, kindInput])
     .then(result => {
+        res.status(200)
         res.send(result);
     })
 })
 
-app.patch('/pets/update', (req, res) => {
-    var petId = req.body.id;
+app.patch('/pets/update/:id', (req, res) => {
+    var petId = req.params.id;
     var nameInput = req.body.name;
     var ageInput = req.body.age;
     var kindInput = req.body.kind;
-    if (nameInput) {
-        client.query(`UPDATE pets SET name = '${nameInput}'  WHERE pets_id = ${petId}`)
-        .then(result => {
-            res.send('success')})
+    if (ageInput != undefined && typeof ageInput !== 'number'){
+        res.status(400)
+        res.send('Age must be a number')
     }
-    else if (ageInput) {
-        client.query(`UPDATE pets SET age = ${ageInput}  WHERE pets_id = ${petId}`)
-        .then(result => {
-            res.send('success')})
-    }
-    else if (kindInput) {
-        client.query(`UPDATE pets SET kind = '${kindInput}'  WHERE pets_id = ${petId}`)
-        .then(result => {
-            res.send('success')})
-    }
-    else {
-        res.send('Incorrect format')
-    }
+    client.query(`UPDATE pets SET name = COALESCE($1, name), age = COALESCE($2, age), kind = COALESCE($3, kind) WHERE pets_id = $4`,[nameInput, ageInput, kindInput, petId])
+    .then(result =>{
+        res.status(200)
+        res.send('Pet has been updated')
+    })
 })
 
 app.delete('/pets/delete/:id', (req, res) => {
-        var petId = req.body.id;
+        var petId = req.params.id;
         client.query(`DELETE FROM pets WHERE pets_id = $1`, [petId])
         .then(result => {
+            res.status(200)
             res.send('Deleted')})
 })
 
